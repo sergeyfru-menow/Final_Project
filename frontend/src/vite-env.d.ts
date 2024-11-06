bc2acc2588a13bc134d60a081885215ecd362019
sergeyfru <sn.fruman@gmail.com>
2024-11-06 11:47:18 +0200

    +

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,7 +2,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_API_URL: string;
+    readonly VITE_API_URL: string | null | undefined;
 }
 
 interface ImportMeta {
